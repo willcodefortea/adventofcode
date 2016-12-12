db477478b369e0b8378bb8c93500ff1db4e4fa79
Ben Emery <ben.s.emery@gmail.com>
2016-12-12 00:39:48 +0000

    Day 11

---
+++ 11.py
@@ -0,0 +1,193 @@
+from collections import namedtuple
+
+
+Move = namedtuple('Move', 'old_floor,new_floor,items')
+
+
+class State(object):
+    def __init__(self, state, elevator=0, depth=1, moves=None):
+        self.state = state
+        self.depth = depth
+        self.elevator = elevator
+        self.moves = moves or []
+
+    def __str__(self):
+        output = []
+        for index, floor in enumerate(self.state):
+            output.append(
+                'F{} {} {}'.format(
+                    index,
+                    'E ' if index == self.elevator else '. ',
+                    ' '.join(floor)
+                )
+            )
+        return '\n'.join(output[::-1])
+
+    @property
+    def key(self):
+        chunks = [
+            '%d%s' % (index, '-'.join(sorted(floor)))
+            for index, floor in enumerate(self.state)
+        ]
+
+        chunks.insert(0, str(self.elevator))
+        return ','.join(chunks)
+
+    def clone(self):
+        return State(
+            [f[:] for f in self.state],
+            depth=self.depth,
+            elevator=self.elevator,
+            moves=self.moves[:]
+        )
+
+    @property
+    def cost(self):
+        """A cost function for our state. Lower is better.
+
+        We're seeking to get all components on the top floor, so describe
+        the cost as the number of sum(no. items * distance) for each
+        possible distance.
+
+        The result is clamped between 0 and 1.
+        """
+        max_cost = sum(len(f) for f in self.state) * (len(self.state) - 1)
+        total = 0
+        for index, floor in enumerate(self.state[::-1]):
+            total += index * len(floor)
+        return 1.0 * total / max_cost
+
+    @property
+    def is_valid(self):
+        """Does anything end up frying in this state?"""
+        for floor in self.state:
+            required_generators = set()
+            generators = set()
+            for item in floor:
+                if item[1] == 'M':
+                    required_generators.add('%sG' % item[0])
+                else:
+                    generators.add(item)
+
+            # If we have a chip and *any* generators without each chip's
+            # corresponding generator, we'll fry the chip
+            if generators:
+                missing_generators = required_generators - generators
+                if missing_generators:
+                    return False
+        return True
+
+    @property
+    def available_moves(self):
+        """Find any and all valid moves that can be made from this state."""
+        floor = self.state[self.elevator]
+
+        for new_floor in (self.elevator - 1, self.elevator + 1):
+            if new_floor < 0 or new_floor >= len(self.state):
+                continue
+
+            # First grab each item, then any grouping of two
+            for index, item in enumerate(floor):
+                yield Move(
+                    self.elevator, new_floor, [item, ]
+                )
+
+                for second_item in floor[index + 1:]:
+                    yield Move(
+                        self.elevator, new_floor, [item, second_item, ]
+                    )
+
+    def apply_move(self, move):
+        """Update our state."""
+        self.state[move.new_floor].extend(move.items)
+        for item in move.items:
+            self.state[move.old_floor].remove(item)
+
+        self.depth += 1
+        self.elevator = move.new_floor
+        self.moves.append(move)
+
+
+def test_part_1():
+    state = State(
+        [
+            ['HM', 'LM'],
+            ['HG', ],
+            ['LG', ],
+            [],
+        ]
+    )
+
+    result = part_1(state)
+    assert result == 11
+
+
+def find_lowest_path(state):
+    """
+    Find the shortest allowed route to move items to the 4th floor.
+
+    This is *not* fast as it looks at each possible state in turn and
+    performs each possible move on each state. Instead rank by the cost
+    function and the number of steps? Follow low cost steps first before
+    attempting other routes?
+    """
+    visited = set()
+    states = [state, ]
+    while True:
+        new_states = []
+        for state in states:
+            for move in state.available_moves:
+                new_state = state.clone()
+                new_state.apply_move(move)
+
+                if new_state.key in visited:
+                    # We've travelled this path before, skip it.
+                    continue
+                visited.add(new_state.key)
+
+                if new_state.is_valid:
+                    # We've found a valid state, explore it
+                    # next time
+                    if new_state.cost == 0:
+                        # We've found the first instance of reaching the
+                        # top! Huzzah.
+                        return state.depth
+                    new_states.append(new_state)
+
+        # Update our states
+        states = sorted(new_states, key=lambda state: state.cost)
+
+
+def part_1(state):
+    return find_lowest_path(state)
+
+
+def main():
+    state = State(
+        [
+            ['PG', 'TG', 'TM', 'PRG', 'RG', 'RM', 'CG', 'CM', ],
+            ['PM', 'PRM', ],
+            [],
+            [],
+        ]
+    )
+    test_part_1()
+    answer = part_1(state)
+    print 'The minimum number of moves is %s.' % answer
+
+    state2 = State(
+        [
+            [
+                'PG', 'TG', 'TM', 'PRG', 'RG', 'RM', 'CG', 'CM', 'EG', 'EM',
+                'DG', 'DM'
+            ],
+            ['PM', 'PRM', ],
+            [],
+            [],
+        ]
+    )
+    answer = part_1(state2)
+    print 'The minimum number of moves is %s.' % answer
+
+if __name__ == '__main__':
+    main()
