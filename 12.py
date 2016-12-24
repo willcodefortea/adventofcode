f2b4e0c8534c8dcf6afd4b3b825bf01a67c9f543
Ben Emery <ben.s.emery@gmail.com>
2016-12-24 00:57:12 +0000

    Rewrite day 12 to be OO

--- 12.py
+++ 12.py
@@ -1,86 +1,84 @@
-def build_registry():
-    return {
-        'a': 0,
-        'b': 0,
-        'c': 0,
-        'd': 0,
-        'instruction': 0,
-    }
+class Interpreter(object):
+    def __init__(self, instructions):
+        self.instructions = instructions
+        self.index = 0
+        self.registry = {
+            'a': 0,
+            'b': 0,
+            'c': 0,
+            'd': 0,
+        }
 
+    def execute(self):
+        """Perform all instructions."""
+        while True:
+            try:
+                instruction = self.instructions[self.index]
+            except IndexError:
+                break
+            self.execute_instruction(instruction)
 
-def _int_or_registry_value(registry, key):
-    """Check if the key is an integer or a registry value."""
-    try:
-        value = int(key)
-    except ValueError:
-        value = registry[key]
-    return value
-
-
-def copy(registry, source, destintion):
-    """Copy either a value or a registry value to another registry."""
-    value = _int_or_registry_value(registry, source)
-    registry[destintion] = value
-
-
-def increase(registry, destintion):
-    """Increase a registry value."""
-    registry[destintion] += 1
-
-
-def decrease(registry, destintion):
-    """Decrease a registry value."""
-    registry[destintion] -= 1
-
-
-def jump(registry, key, amount):
-    """Jump to a previous instruction if the flag is set."""
-    value = _int_or_registry_value(registry, key)
-    amount = int(amount)
-    if value != 0:
-        registry['instruction'] += amount
-
-
-OPERATIONS = {
-    'cpy': copy,
-    'inc': increase,
-    'dec': decrease,
-    'jnz': jump,
-}
-
-
-def execute_instruction(instruction, registry):
-    """Parse the instruction and perform the required operation."""
-    chunks = instruction.split(' ')
-    name = chunks[0]
-    arguments = chunks[1:]
-
-    try:
-        operation = OPERATIONS[name]
-    except KeyError:
-        raise Exception('Unknown instruction "%s".' % name)
-
-    # Keep track of the previous instruction index in case the operation
-    # changes it.
-    prev_instruction_index = registry['instruction']
-
-    # Perform the operation with any remaining arguments from the instruction
-    operation(registry, *arguments)
-
-    if prev_instruction_index == registry['instruction']:
-        # The instruction did NOT jump to a different location, advance it
-        # ourselves
-        registry['instruction'] += 1
-
-
-def execute_instructions(instructions, registry):
-    """Continually perform instructions until we run out."""
-    while True:
+    def _int_or_registry_value(self, key):
+        """Check if the key is an integer or a registry value."""
         try:
-            instruction = instructions[registry['instruction']]
-        except IndexError:
-            break
-        execute_instruction(instruction, registry)
+            value = int(key)
+        except ValueError:
+            value = self.registry[key]
+        return value
+
+    def copy(self, source, destintion):
+        """Copy either a value or a registry value to another registry."""
+        value = self._int_or_registry_value(source)
+        self.registry[destintion] = value
+
+    def increase(self, destintion):
+        """Increase a registry value."""
+        self.registry[destintion] += 1
+
+    def decrease(self, destintion):
+        """Decrease a registry value."""
+        self.registry[destintion] -= 1
+
+    def jump(self, key, amount):
+        """Jump to a previous instruction if the flag is set."""
+        value = self._int_or_registry_value(key)
+        amount = int(amount)
+        if value != 0:
+            self.index += amount
+
+    @property
+    def operations_map(self):
+        """Dumb instruction key to method mapping."""
+        return {
+            'cpy': self.copy,
+            'inc': self.increase,
+            'dec': self.decrease,
+            'jnz': self.jump,
+        }
+
+    def execute_instruction(self, instruction):
+        """Parse the instruction and perform the required operation."""
+        chunks = instruction.split(' ')
+        name = chunks[0]
+        arguments = chunks[1:]
+
+        try:
+            operation = self.operations_map[name]
+        except KeyError:
+            raise Exception('Unknown instruction "%s".' % name)
+
+        # Keep track of the previous instruction index in case the operation
+        # changes it.
+        prev_instruction_index = self.index
+
+        # Perform the operation with any remaining arguments from the
+        # instruction
+        operation(*arguments)
+
+        if prev_instruction_index == self.index:
+            # The instruction did NOT jump to a different location, advance it
+            # ourselves
+            self.index += 1
 
 
 def test_part_1():
@@ -92,9 +90,9 @@ def test_part_1():
         'jnz a 2',
         'dec a',
     ]
-    registry = build_registry()
-    execute_instructions(instructions, registry)
-    assert registry['a'] == 42
+    interpreter = Interpreter(instructions)
+    interpreter.execute()
+    assert interpreter.registry['a'] == 42
 
 
 def main():
@@ -102,15 +100,15 @@ def main():
         instructions = fin.read().split('\n')
     test_part_1()
 
-    registry = build_registry()
-    execute_instructions(instructions, registry)
-    answer = registry['a']
+    interpreter = Interpreter(instructions)
+    interpreter.execute()
+    answer = interpreter.registry['a']
     print 'The value in register a is %s.' % answer
 
-    registry = build_registry()
-    registry['c'] = 1
-    execute_instructions(instructions, registry)
-    answer = registry['a']
+    interpreter = Interpreter(instructions)
+    interpreter.registry['c'] = 1
+    interpreter.execute()
+    answer = interpreter.registry['a']
     print 'The value in register a is %s.' % answer
 
 
